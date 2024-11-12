6229b47769f9dcebbc741aac24e543e7e404114b
bedvoa <ujmn0418@gmail.com>
2024-11-13 00:48:31 +0900

    feat: app settings

---
+++ first-my-app/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Test!!!!!🚨</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
