2a7933e3d2824823c1c26eb77a7038d762d2d63f
bedvoa <ujmn0418@gmail.com>
2024-11-15 02:04:14 +0900

    project settings

--- first-my-app/app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TabTwoScreen() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Text>Test!!!!!🚨</Text>
-    </View>
-  );
-}
